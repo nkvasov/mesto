12e488273edd709139d04500f570e714d0eebeee
Nikolay Kvasov <nkvasov@yandex.ru>
2020-06-12 18:47:52 +0300

    feat: JS done

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 const popup = document.querySelector('.popup');
 const popupCloseBtn = popup.querySelector('.popup__close-btn');
-const profileEditBtn = document.querySelector('.profile__edit-btn');
+
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__name');
+const profileJob = profile.querySelector('.profile__description');
+const profileEditBtn = profile.querySelector('.profile__edit-btn');
+
+const popupForm = popup.querySelector('.popup__container');
+const nameInput = popupForm.querySelector('.popup__name-input');
+const jobInput = popupForm.querySelector('.popup__job-input');
 
 popupCloseBtn.addEventListener('click', popupToggle);
-profileEditBtn.addEventListener('click', popupToggle);
+
+profileEditBtn.addEventListener('click', editProfile);
+
+popupForm.addEventListener('submit', formSubmitHandler);
+
 
 
 function popupToggle() {
   popup.classList.toggle('popup_opened');
 }
 
+function editProfile() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popupToggle();
+}
 
-
-// Находим форму в DOM
-let popupForm = popup.querySelector('.popup__container');
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = popupForm.querySelector('.popup__name-input');
-    let jobInput = popupForm.querySelector('.popup__job-input');
-
-    // Получите значение полей из свойства value
-    let name = nameInput.getAttribute('value');
-    console.log({name});
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popupToggle();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-popupForm.addEventListener('submit', formSubmitHandler);
