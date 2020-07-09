7f1adab8be1e72607793c269a53b085876deef69
Nikolay Kvasov <nkvasov@yandex.ru>
2020-07-10 00:18:12 +0300

    feat: add form validation befor input

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,7 +4,13 @@ const mesto = {
   submitButtonSelector: '.form__submit-btn',
   // inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: '.form__input-error',
-  errorClass: 'form__input-error_active'
+  errorClass: 'form__input-error_active',
+
+  // добавил в объект свойства (имена форм и классы кнопок их открывающих)
+  formName1: 'edit-profile',
+  formBtnClass1: '.profile__edit-btn',
+  formName2: 'add-card',
+  formBtnClass2: '.add-btn',
 }
 
 function showError(inputElement, errorMessage, page) {
@@ -21,11 +27,8 @@ function hideError(inputElement, page) {
   errorElement.textContent = '';
 }
 
-// function toggleButtonState(buttonElement) {
-//   buttonElement.classList.toggle('button_inactive');
-// }
 
-// Проверяет валидность одного поля ввода
+// Проверяет валидность одного поля ввода и показывает/убирает сообщение об ошибке
 function checkInputValidity(inputElement, page) {
   if (inputElement.validity.valid) {
     hideError(inputElement, page);
@@ -74,6 +77,7 @@ function setButtonState(formElement, buttonElement, page) {
 // Навешивает валидирующих слушателей на инпуты формы
 function addFormListeners(formElement, page) {
   const submitBtnElement = formElement.querySelector(page.submitButtonSelector);
+  // setButtonState(formElement, submitBtnElement, page);
   returnInputList(formElement, page).forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       checkInputValidity(inputElement, page);
@@ -82,17 +86,38 @@ function addFormListeners(formElement, page) {
   })
 }
 
+// Проверяет валидность формы: проверяет все поля, включает сообщения об ошибках, меняет состояние кнопки submit
+function checkFormValidity(formElement, page) {
+  const submitBtnElement = formElement.querySelector(page.submitButtonSelector);
+  returnInputList(formElement, page).forEach((inputElement) => {
+    checkInputValidity(inputElement, page);
+  })
+  setButtonState(formElement, submitBtnElement, page);
+}
 
 
 function enableValidation(page) {
   const formList = Array.from(document.forms);
+  const form1 = document.forms[page.formName1];
+  const form2 = document.forms[page.formName2];
+  const openBtn1 = document.querySelector(page.formBtnClass1);
+  const openBtn2 = document.querySelector(page.formBtnClass2);
+
   formList.forEach((formElement) => {
     addFormListeners(formElement, page);
+  })
+  // Эти слушатели вешаются на кнопки открытия форм. Они проверяют валидность формы при каждом открытии.
+  // И устанавливают правильное состояние кнопки submit и ошибок инпутов до события input.
+  // Иначе при открытии формы могло остаться неправильное состояние элементов от события input при прошлом открытии, например при выходе через ESC
+  openBtn1.addEventListener('click', () => {
+    checkFormValidity(form1, page);
+  })
+  openBtn2.addEventListener('click', () => {
+    checkFormValidity(form2, page);
   })
 }
 
 enableValidation(mesto);
-
 
 
 
